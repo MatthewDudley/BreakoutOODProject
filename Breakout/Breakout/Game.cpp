f8582281ff0f01522a3cdbf1172d25e8d277e055
Mitchell Hodzen <mitchellhodzen@yahoo.com>
2017-10-11 11:19:21 -0400

    Initial Commit C++ and SDL2 window rendering, and SDL_img set up to make loading PNG files easier in the future

---
+++ Breakout/Breakout/Game.cpp
@@ -0,0 +1,103 @@
+#include "Game.h"
+//#include "Animation.h"
+
+
+Game::Game()
+{
+	screenWidth = 800;
+	screenHeight = 601;
+	levelWidth = 1000;
+	levelHeight = 1001;
+
+	renderer = new Renderer(screenWidth, screenHeight, false);
+	//mediaManager = new MediaManager();
+	//inputManager = new InputManager();
+}
+
+
+Game::~Game()
+{
+	delete renderer;
+	//delete mediaManager;
+	//delete inputManager;
+}
+
+void Game::Start()
+{
+	if (Initialize())
+	{
+		bool quit = false;
+		/*
+		Player player(350, 150, 18, 26, -9.0f, -12.0f);
+		player.GetAnimationController()->SetTexture(mediaManager->GetTexture(0));
+		player.GetAnimationController()->SetAnimationMap(mediaManager->GetPlayerAnimations());
+		player.Initialize();
+		
+		BlockTest block(350, 345, 100, 100, -50.0f, -50.0f, false);
+		block.GetVisualComponent()->SetTexture(mediaManager->GetTexture(1));
+		block.GetSingleImageController()->SetCurrentSpriteRect(100, 0, 100, 100);
+
+
+		std::vector<Entity*> entityList;
+		entityList.push_back(&block);
+		entityList.push_back(&block2);
+		entityList.push_back(&block3);
+		entityList.push_back(&block4);
+		entityList.push_back(&block5);
+		entityList.push_back(&block6);
+		entityList.push_back(&block7);
+		entityList.push_back(&block8);
+		entityList.push_back(&block9);
+		entityList.push_back(&block10);
+		entityList.push_back(&block11);
+		entityList.push_back(&block12);
+		entityList.push_back(&block13);
+		entityList.push_back(&block14);
+		entityList.push_back(&block15);
+		entityList.push_back(&player);
+
+
+		Camera camera(0, 0, screenWidth, screenHeight, levelWidth, levelHeight);
+		camera.SetTarget(&player);
+		*/
+
+		Uint32 lastFrameTime = 0;
+		Uint32 currentFrameTime = 0;
+		float deltaTime = 0.0f;
+		//renderer->Draw(&entityList, &camera);
+		currentFrameTime = SDL_GetTicks();
+		while (quit == false)
+		{
+			lastFrameTime = currentFrameTime;
+			currentFrameTime = SDL_GetTicks();
+			deltaTime = (float)(currentFrameTime - lastFrameTime) / 1000;
+			//player.GetTiming(lastFrameTime, currentFrameTime, deltaTime);
+			//quit = inputManager->HandleInput(deltaTime, &player);
+			//player.Update(entityList);
+			//camera.Reset();
+			renderer->Draw();//&entityList, &camera);
+			//SDL_Delay(30);
+		}
+	}
+}
+
+bool Game::Initialize()
+{
+	if (renderer->Initialize() == true)
+	{
+		/*
+		mediaManager->CreateTexture("Media/Sprites/megamanx.png", renderer->GetRenderer());
+		//mediaManager->CreateTexture("Media/Sprites/megamanxidlerun.png", renderer->GetRenderer());
+		//mediaManager->CreateTexture("Media/Sprites/runningplayertransparencytest.png", renderer->GetRenderer());
+		mediaManager->CreateTexture("Media/Sprites/blocktypes.png", renderer->GetRenderer());
+		mediaManager->CreateTexture("Media/Sprites/player1.png", renderer->GetRenderer());
+		mediaManager->CreateTexture("Media/Sprites/block1.png", renderer->GetRenderer());
+		mediaManager->GeneratePlayerAnimations();
+		*/
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
---
+++ Breakout/Breakout/Game.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <SDL.h>
+#include <stdio.h>
+#include "Renderer.h"
+/*
+#include "Entity.h"
+#include "Player.h"
+#include "BlockTest.h"
+#include <vector>
+#include "Texture.h"
+#include "MediaManager.h"
+#include "Camera.h"
+#include "InputManager.h"
+#include "AnimationNames.h"
+*/
+class Game
+{
+public:
+	Game();
+	~Game();
+	void Start();
+private:
+	bool Initialize();
+	int screenWidth;
+	int screenHeight;
+	int levelWidth;
+	int levelHeight;
+
+	Renderer* renderer;
+	//MediaManager* mediaManager;
+	//InputManager* inputManager;
+
+};
+
