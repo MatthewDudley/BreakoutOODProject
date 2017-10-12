21b88e8db91b484ac717be4024cdf84ced319580
Mitchell Hodzen <mitchellhodzen@yahoo.com>
2017-10-11 20:31:13 -0400

    Changed vector class to be header only

--- Breakout/Breakout/Vector2.h
+++ Breakout/Breakout/Vector2.h
@@ -3,14 +3,39 @@
 class Vector2
 {
 public:
-	Vector2(float x = 0.0f, float y = 0.0f);
-	~Vector2();
-	float GetX();
-	float GetY();
-	void SetX(float x);
-	void SetY(float y);
-	void Add(float x, float y);
-	static bool CheckEqual(Vector2& v1, Vector2& v2);
+	Vector2(float x = 0.0f, float y = 0.0f)
+	{
+		//If no value is given, X and Y default to 0
+		internalArray[0] = x;
+		internalArray[1] = y;
+	}
+
+	~Vector2()
+	{
+		delete[] internalArray;
+	}
+
+	float GetX() { return internalArray[0]; }
+	float GetY() { return internalArray[1]; }
+
+	void SetX(float x) { internalArray[0] = x; }
+	void SetY(float y) { internalArray[1] = y; }
+
+	void Add(float x, float y)
+	{
+		internalArray[0] += x;
+		internalArray[1] += y;
+	}
+
+	static bool CheckEqual(Vector2& v1, Vector2& v2)
+	{
+		if (v1.GetX() == v2.GetX() && v1.GetY() == v2.GetY())
+		{
+			return true;
+		}
+		return false;
+	}
+
 private:
 	float* internalArray = new float[2];
 };
